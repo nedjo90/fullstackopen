49d19883772290d34ce902c53def0ae3ed0f8137
necatihan <han.necati@gmail.com>
2024-05-27 14:41:35 +0200

    finished course info exercises

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,7 +8,11 @@ const Header = (props) =>
 const Content = (props) =>
 {
     return (
-        <p>{props.part} {props.exercises}</p>
+        <div>
+            <p>{props.part[0]} {props.exercises[0]}</p>
+            <p>{props.part[1]} {props.exercises[1]}</p>
+            <p>{props.part[2]} {props.exercises[2]}</p>
+        </div>
     )
 }
 
@@ -31,11 +35,8 @@ const App = () => {
     return (
         <div>
             <Header title={course} />
-            <Content  part={part1} exercises={exercises1} />
-            <Content  part={part2} exercises={exercises2} />
-            <Content  part={part3} exercises={exercises3} />
+            <Content  part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
             <Total total={exercises1 + exercises2 + exercises3} />
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
         </div>
     )
 }
