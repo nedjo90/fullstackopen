3a1f1fcd852fcdfb7e8d2470b428acd1c76ed405
necatihan <han.necati@gmail.com>
2024-05-30 13:57:07 +0200

    finished altering data

--- other/datafromserver/src/App.jsx
+++ other/datafromserver/src/App.jsx
@@ -1,28 +1,62 @@
-import './App.css'
 import axios from "axios";
 import {
   useEffect,
   useState
 } from "react";
+import Content from "./components/content.jsx";
+import noteService from './services/notes.js'
 
 function App() {
   const[notes, setNotes] = useState([]);
   const[newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-      console.log('effect');
-      axios.get('http://localhost:3001/notes')
-          .then(response => {
-              console.log('promise fulfilled');
-              setNotes(response.data)
-          });
-  };
+  const addNotes = event => {
+      event.preventDefault();
+      const noteObject = {
+          content: newNote,
+          important: Math.random() < 0.5
+      }
+      noteService.create(noteObject)
+          .then(returnedNote =>{
+              setNotes(notes.concat(returnedNote));
+              setNewNote('');
+          })
+  }
 
-  useEffect(hook, []);
-  console.log('render', notes.length, 'notes');
+  useEffect(() => {
+      noteService.getAll()
+          .then(initialNotes => {
+              setNotes(initialNotes);
+          });
+  }, []);
+  const handleNoteChange = (event) => setNewNote(event.target.value);
+  const handleShowChange = () => setShowAll(!showAll);
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+
+    const toggleChangeImportant = (id) =>
+    {
+        console.log(id);
+        const note = notes.find(note => note.id === id);
+        const updatedNote = {...note, important: !note.important};
+        noteService.update(id, updatedNote)
+            .then(returnedNote =>{
+                setNotes(notes.map(n => n.id !== id ? n : returnedNote));
+            })
+            .catch(error => {
+                alert(`the note '${note.content}' was already delete from server`)
+            });
+    }
+
   return (
-    <div></div>
+      <div>
+          <input type="text" onChange={handleNoteChange}/>
+          <button onClick={addNotes}>save</button>
+          <input type="checkbox" onChange={handleShowChange}/>
+          <label >Only important</label>
+          <Content notes={notesToShow} toggle={toggleChangeImportant}/>
+      </div>
   )
 }
 
---
+++ other/datafromserver/src/components/content.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+const Content = ({notes, toggle}) => {
+    return(
+        <div>
+            {
+                notes.map(note =>
+                    <li key={note.id}>
+                        {note.content}
+                        <button onClick={()=>toggle(note.id)}>{note.important ? 'important' : 'not' +
+                            ' important'}</button>
+                    </li>
+                )
+            }
+        </div>
+    )
+}
+
+export default Content;
